bb2386a5222f6b87515258425fb3b02432012219
MONIR <MONIR>
2018-03-22 13:14:22 +0600

    Change greetings method

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello{
     public static void main(String[] args){
         Sytem.out.println("Git is Awesome");
+        greetings("Git");
     }
 
     public static void greetings(String name){
-        Sytem.out.println("Welcome "+name);
+        Sytem.out.println("Welcome " + name);
     }
 
 }
