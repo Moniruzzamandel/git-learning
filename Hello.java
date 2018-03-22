c36a62f4e3062a6e230a301d6f73d25137f977d8
MONIR <MONIR>
2018-03-22 11:36:18 +0600

    New method is add to our Hello.java class

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ public class Hello{
     public static void main(String[] args){
         Sytem.out.println("Git is Awesome");
     }
+
+    public static void greetings(String name){
+        Sytem.out.println("Welcome "+name);
+    }
+
 }
